27761708887c912241e2ee81b58a64ff95b94b80
Nik Deapen <nik@deapen.com>
2023-12-18 06:11:27 -0800

    fmt

--- src/extract/parser.rs
+++ src/extract/parser.rs
@@ -5,7 +5,6 @@ use crate::extract::webpage::Webpage;
 
 /// Responsible for parsing webpage content.
 pub trait Parser<T> {
-
     /// Parses data from the webpage.
     fn parse(&self, webpage: &Webpage, html: &Html) -> Result<T, Error>;
 }
